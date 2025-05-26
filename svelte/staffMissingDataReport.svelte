33c129fb09360d93b199f0e771c94d9f528c47a2
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2025-05-26 19:33:40 +0800

    [in-progress] page missing data report

--- svelte/staffMissingDataReport.svelte
+++ svelte/staffMissingDataReport.svelte
@@ -6,14 +6,53 @@
 
   let user      = /** @type {User} */ ({/* user */});
   let segments  = /** @type {Access} */ ({/* segments */});
+  let tenants     = /** @type {Record<Number, string>} */({/* tenants */});
 </script>
-
 
 <LayoutMain access={segments} user={user}>
   <div class="report-container">
-    <h1>Missing data report</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Room</th>
+          <th>Tenant Name</th>
+          <th>Telegram</th>
+          <th>WhatsApp</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
   </div>
 </LayoutMain>
 
 <style>
+  .report-container {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    padding: 20px;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  table thead {
+    position: sticky;
+    top: 0;
+    background-color: var(--gray-002);
+    z-index: 10;
+    border: none;
+  }
+
+  table thead tr th {
+    text-align: left;
+    padding: 8px 12px;
+  }
+
+  table tbody tr {
+    border-bottom: 1px solid var(--gray-004);
+  }
 </style>
