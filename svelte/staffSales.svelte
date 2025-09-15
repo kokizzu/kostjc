f6f2984a0fd18e172db6ca5799c75fa28dbe187f
khairulharu <khairulaswad719@gmail.com>
2025-09-15 12:44:41 +0800

    fix api adminSale to staffSales

--- svelte/staffSales.svelte
+++ svelte/staffSales.svelte
@@ -9,7 +9,7 @@
 
   import LayoutMain from './_layouts/main.svelte';
   import MasterSales from './_components/MasterSales.svelte';
-  import { AdminSale } from './jsApi.GEN';
+  import { AdminSale, StaffSales } from './jsApi.GEN';
   import { CmdList, CmdUpsert, CmdUpdatePayment } from './_components/xConstant';
   import { notifier } from './_components/xNotifier';
     
@@ -26,8 +26,8 @@
       pager: pagerIn,
       cmd: CmdList
     };
-    await AdminSale( // @ts-ignore
-      i, /** @type {import('./jsApi.GEN').AdminSaleCallback} */
+    await StaffSales( // @ts-ignore
+      i, /** @type {import('./jsApi.GEN').StaffSalesCallback} */
       /** @returns {Promise<void>} */
       function(/** @type any */ o) {
         if (o.error) {
@@ -50,8 +50,8 @@
       cmd: CmdUpsert
     });
 
-    await AdminSale(i,
-      /** @type {import('../jsApi.GEN').AdminSaleCallback} */
+    await StaffSales(i,
+      /** @type {import('../jsApi.GEN').StaffSalesCallback} */
       /** @returns {Promise<void>} */
       function(/** @type any */ o) {
         if (o.error) {
@@ -87,8 +87,8 @@
       sale,
       cmd: CmdUpdatePayment
     });
-    await AdminSale(i,
-      /** @type {import('./jsApi.GEN').AdminSaleCallback} */
+    await StaffSales(i,
+      /** @type {import('./jsApi.GEN').StaffSalesCallback} */
       /** @returns {Promise<void>} */
       function(/** @type any */ o) {
         if (o.error) {
